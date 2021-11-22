350120adf85112d5cb67fede19a078ad0d71af05
leapig <1509977191@qq.com>
2021-11-22 16:00:54 +0800

    新增workflowForm

--- table/WorkflowFrom.go
+++ table/WorkflowFrom.go
@@ -1,4 +1,21 @@
 package table
 
+import "time"
+
 type WorkflowForm struct {
+	Id         int       `json:"id"`
+	Uuid       string    `json:"uuid"`
+	WfUuid     string    `json:"wf_uuid"`
+	Type       string    `json:"type"`
+	Title      string    `json:"title"`
+	Name       string    `json:"name"`
+	Default    string    `json:"default"`
+	Multiple   uint8     `json:"multiple"`
+	Required   uint8     `json:"required"`
+	List       string    `json:"list"`
+	Sort       uint8     `json:"sort"`
+	IsDelete   uint8     `json:"is_delete"`
+	CreateTime time.Time `json:"create_time"`
+	UpdateTime time.Time `json:"update_time"`
+	DeleteTime time.Time `json:"delete_time"`
 }
